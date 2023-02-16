a7b6fc1aa8ad031b4ad3e1c634dd48bb71970c53
Patricks740 <patrick.s740@googlemail.com>
2023-02-16 16:02:09 +0100

    1 file modified

--- App.tsx
+++ App.tsx
@@ -18,6 +18,23 @@ export default function App() {
     return { ...user, ...overrides };
   };
 
+  type RequiredMyUser = Required<myUser>;
+
+  type JustEmailAndName = Pick<myUser, 'name' | 'email'>;
+
+  type UserWithoutId = Omit<myUser, 'id'>;
+
+  //                                                    <UserWithoutId>
+  const mapById = (users: myUser[]): Record<string, Omit<myUser, 'id'>> => {
+    return users.reduce((a, v) => {
+      const { id, ...other } = v;
+      return {
+        ...a,
+        [id]: other,
+      };
+    }, {});
+  };
+
   return (
     <div>
       <h1>Utility Types</h1>
@@ -31,6 +48,20 @@ export default function App() {
             },
             { email: 'Jack.foo@gmail.com' }
           )
+        )}
+      </p>
+      <p>
+        {JSON.stringify(
+          mapById([
+            {
+              id: 'foo',
+              name: 'Jack',
+            },
+            {
+              id: 'baz',
+              name: 'Ann',
+            },
+          ])
         )}
       </p>
     </div>
